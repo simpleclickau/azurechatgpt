cf8ba0f405e6e2222bf547a6a29a45ee6c280de6
Arve Solland <arvesolland@gmail.com>
2023-08-07 16:33:26 +1000

    test

--- src/features/chat/chat-ui/chat-ui.tsx
+++ src/features/chat/chat-ui/chat-ui.tsx
@@ -16,95 +16,95 @@ import { transformCosmosToAIModel } from "../chat-services/utils";
 import { ChatHeader } from "./chat-header";
 
 interface Prop {
-  chats: Array<ChatMessageModel>;
-  model: string;
+    chats: Array<ChatMessageModel>;
+    model: string;
 }
 
 export const ChatUI: FC<Prop> = (props) => {
-  const { id } = useParams();
-  const { data: session } = useSession();
-  const [chatBody, setBody] = useState<PromptGPTBody>({
-    id: id,
-    model: "GPT-3.5",
-    chatType: "simple",
-  });
-
-  const { toast } = useToast();
-  const {
-    messages,
-    input,
-    handleInputChange,
-    handleSubmit,
-    reload,
-    isLoading,
-  } = useChat({
-    onError,
-    id,
-    body: chatBody,
-    initialMessages: transformCosmosToAIModel(props.chats),
-  });
-
-  const scrollRef = useRef<HTMLDivElement>(null);
-  useChatScrollAnchor(messages, scrollRef);
-
-  function onError(error: Error) {
-    toast({
-      variant: "destructive",
-      description: error.message,
-      action: (
-        <ToastAction
-          altText="Try again"
-          onClick={() => {
-            reload();
-          }}
-        >
-          Try again
-        </ToastAction>
-      ),
+    const { id } = useParams();
+    const { data: session } = useSession();
+    const [chatBody, setBody] = useState<PromptGPTBody>({
+        id: id,
+        model: "GPT-3.5",
+        chatType: "simple",
     });
-  }
 
-  const onValueChange = (value: string) => {
-    setBody((e) => ({ ...e, model: value }));
-  };
+    const { toast } = useToast();
+    const {
+        messages,
+        input,
+        handleInputChange,
+        handleSubmit,
+        reload,
+        isLoading,
+    } = useChat({
+        onError,
+        id,
+        body: chatBody,
+        initialMessages: transformCosmosToAIModel(props.chats),
+    });
 
-  const onHandleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    handleSubmit(e);
-  };
+    const scrollRef = useRef<HTMLDivElement>(null);
+    useChatScrollAnchor(messages, scrollRef);
 
-  return (
-    <Card className="h-full relative">
-      <div className="h-full rounded-md overflow-y-auto" ref={scrollRef}>
-        <div className="flex justify-center p-4">
-          <ChatHeader
-            isEnabled={messages.length !== 0}
-            chatType="data"
-            onValueChange={(chatType) => {}}
-          />
-        </div>
-        <div className=" pb-[80px] ">
-          {messages.map((message, index) => (
-            <ChatRow
-              name={
-                message.role === "user" ? session?.user?.name! : "AzureChatGPT"
-              }
-              profilePicture={
-                message.role === "user" ? session?.user?.image! : "/ai-icon.png"
-              }
-              message={message.content}
-              type={message.role}
-              key={index}
+    function onError(error: Error) {
+        toast({
+            variant: "destructive",
+            description: error.message,
+            action: (
+                <ToastAction
+                    altText="Try again"
+                    onClick={() => {
+                        reload();
+                    }}
+                >
+                    Try again
+                </ToastAction>
+            ),
+        });
+    }
+
+    const onValueChange = (value: string) => {
+        setBody((e) => ({ ...e, model: value }));
+    };
+
+    const onHandleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        handleSubmit(e);
+    };
+
+    return (
+        <Card className="h-full relative">
+            <div className="h-full rounded-md overflow-y-auto" ref={scrollRef}>
+                <div className="flex justify-center p-4">
+                    <ChatHeader
+                        isEnabled={messages.length !== 0}
+                        chatType="data"
+                        onValueChange={(chatType) => { }}
+                    />
+                </div>
+                <div className=" pb-[80px] ">
+                    {messages.map((message, index) => (
+                        <ChatRow
+                            name={
+                                message.role === "user" ? session?.user?.name! : "FuseChatGPT"
+                            }
+                            profilePicture={
+                                message.role === "user" ? session?.user?.image! : "/ai-icon.png"
+                            }
+                            message={message.content}
+                            type={message.role}
+                            key={index}
+                        />
+                    ))}
+                    {isLoading && <ChatLoading />}
+                </div>
+            </div>
+            <ChatInput
+                isLoading={isLoading}
+                value={input}
+                handleInputChange={handleInputChange}
+                handleSubmit={onHandleSubmit}
             />
-          ))}
-          {isLoading && <ChatLoading />}
-        </div>
-      </div>
-      <ChatInput
-        isLoading={isLoading}
-        value={input}
-        handleInputChange={handleInputChange}
-        handleSubmit={onHandleSubmit}
-      />
-    </Card>
-  );
+        </Card>
+    );
 };
